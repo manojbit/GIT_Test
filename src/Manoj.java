81a86972eb98ccdb774adc887b07fba14bd3e901
Manoj <manojbit1991@gmail.com>
2023-07-17 18:34:08 +0530

    7th commit by manoj

--- src/Manoj.java
+++ src/Manoj.java
@@ -1,4 +1,10 @@
 public class Manoj {
 
+    private String ename;
+    private int number;
+
+    public static void main(String[] args) {
+        System.out.println("hello bhai");
+    }
 // hi manoj
 }
